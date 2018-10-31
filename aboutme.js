f025f40f8558d3f74dee1c898f27d3f104f0dda4
Ivan <i.poblete915@gmail.com>
2018-10-30 22:16:10 -0700

    fixed js file

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,5 @@
 /* exported checkAboutMe, resetAboutMe */
-
+'use strict';
 var aboutMeForm = document.getElementById('about-me-form');
 var aboutMeResponse = document.getElementById('about-me-response');
 
@@ -10,7 +10,7 @@ function checkAboutMe() {
     var likePizza = pizzaInput.value;
 
     var correct = 0;
-    console.log('pizza');
+    //console.log('pizza');
     if(likePizza.toLowerCase() === 'yes') {
         // let's run this code if i like pizza
         correct = correct + 1;
@@ -20,17 +20,21 @@ function checkAboutMe() {
     }
     else if(likePizza !== '') {
         pizzaInput.classList.add('incorrect');
+        pizzaInput.classList.remove('correct');
     }
+
+    aboutMeResponse.textContent = 'You got ' + correct + ' out of 4';
 }
 
 function resetAboutMe() {
     var elements = aboutMeForm.elements;
     var pizzaInput = elements.pizza;
-    var likePizza = pizzaInput.value;
+    //var likePizza = pizzaInput.value;
 
     pizzaInput.classList.remove('correct');
     pizzaInput.classList.remove('incorrect');
 
     aboutMeResponse.textContent = '';
+
 }
 
--- elementcounter.js
+++ elementcounter.js
@@ -5,15 +5,15 @@
 // eslint-disable-next-line
     
 
-function tryQuerySelector() {
-    var tags = document.querySelectorAll('input[name="tags"]:checked');
+// // function tryQuerySelector() {
+//     var tags = document.querySelectorAll('input[name="tags"]:checked');
 
-    for(var i = 0; i < tags.length; i++) {
-        var input = tags.Count[i];
+//     for(var i = 0; i < tags.length; i++) {
+//         var input = tags.Count[i];
 
-        console.log(input.value, input.checked);
-    if()
+//         console.log(input.value, input.checked);
+//     if()
     
-    }
+//     }
 
-}
+// }
